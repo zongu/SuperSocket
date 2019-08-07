3d628ea1010e397355128f060e0874dfa5185c9c
Carter <zongu3317@gmail.com>
2019-08-07 16:57:59 +0800

    [update] Server端邏輯

--- SuperSocket/Program.cs
+++ SuperSocket/Program.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace SuperSocket
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using SuperSocket.Compoment;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var process = new ServerProcessCompoment();
+            process.Start();
+            process.Stop();
         }
     }
 }
